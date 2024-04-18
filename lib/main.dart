8f2a141f5a08e70e070ccb6a4211bd318fba2a7a
atjoao <130053141+atjoao@users.noreply.github.com>
2024-04-18 16:41:28 +0100

    oop

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSwatch().copyWith(
         primary: const Color.fromARGB(255, 50, 50, 50),
