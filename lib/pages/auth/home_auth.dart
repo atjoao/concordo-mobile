7307491523ed694ca0ea89b08058796c409ad887
atjoao <130053141+atjoao@users.noreply.github.com>
2024-04-18 08:31:46 +0100

    aaaa

---
+++ lib/pages/auth/home_auth.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeAuth extends StatelessWidget {
+  const HomeAuth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
